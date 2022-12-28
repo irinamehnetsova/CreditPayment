f98f6ec3e0dcce04d426190a7b4462f1cb72552b
IrinaMehnetsova <i7417m@gmail.com>
2022-12-28 12:16:30 +0300

    Change commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,13 @@
 public class CreditPaymentService {
-    public double calculate(double credit, double period, double percent){
+    public double calculate(double credit, double period, double percent) {
         double rate = percent / 12;
         double term = period * 12;
-        double coefficient = (rate * Math.pow(1 + rate,term))/(Math.pow(1 + rate, term)-1);
+        double coefficient = (rate * Math.pow(1 + rate, term)) / (Math.pow(1 + rate, term) - 1);
         double payment = credit * coefficient;
         return payment;
+
     }
+
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,25 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         double credit = 1_000_000;
-        double period = 12;
+        double period = 1;
         double percent = 0.0999;
 
         double payment = service.calculate(credit, period, percent);
-        System.out.println("Сумма кредита:" + credit
-                + "Ежемесячный платёж:" + payment + "руб" + percent +
-                "Срок кредита" + period);
+
+        System.out.println("Сумма кредита:" + credit);
+        System.out.println("Ежемесячный платёж:" + payment + "руб" + percent);
+        System.out.println("Срок кредита:" + period);
+
+        System.out.println();
+        System.out.println("Ежемесячный платёж:");
+        System.out.println(service.calculate(1_000_000,2,0.0999));
+
+        System.out.println();
+        System.out.println("Ежемесячный платёж:");
+        System.out.println(service.calculate(1_000_000, 3, 0.0999));
+
+
+
 
     }
 }
